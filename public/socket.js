3bb065c0d807007f0a9b06816d1befd9d46d3feb
Kevin Petitnicolas-Yoeusley <46431396+PYKEngine@users.noreply.github.com>
2019-08-04 11:24:06 +0200

    Update socket.js

--- public/socket.js
+++ public/socket.js
@@ -1,14 +1,19 @@
-let socket = io.connect(`http://localhost:5555`);
+let socket = io.connect(`http://localhost:5555`); // Or https://pykengine.io on prod ENV
 
+// Lunch the game
 function init() {
+  // draw canvas elements
   draw()
+  // send player name to the server that will be used to generate player data and config
   socket.emit("init", {
     playerName: player.name
   });
 }
 
+// on server respond, fetch viruses datas for client side puposes (draw())
 socket.on("initReturn", data => {
   viruses = data.viruses;
+  // send player position to the server every frame 60 time a second (for the server to update players position, prevent hack)
   setInterval(() => {
     if (player.xVector) {
       socket.emit("tick", {
@@ -19,32 +24,33 @@ socket.on("initReturn", data => {
   }, 1000 / 60);
 });
 
+// fetch updated players data from the server
 socket.on("tock", data => {
   players = data.players;
 });
 
+// Fetch updated viruses data (when a new virus is created)
 socket.on("virusSwitch", data => {
   viruses.splice(data.virusIndex, 1, data.newVirus);
 });
 
+// fetch updated players position
 socket.on("tickTock", data => {
   player.locX = data.playerX;
   player.locY = data.playerY;
 });
 
+// Fetch players score and manage DOM
 socket.on("updateLeaderBoard", (data) => {
   document.querySelector(".leader-board").innerHTML = ""
-  document.querySelector(".player-score").innerHTML = ""
   data.forEach((curPlayer) => {
     document.querySelector(".leader-board").innerHTML += `
     <li class="leaderboard-player">${curPlayer.name} - ${curPlayer.score}</li>
     `
-    document.querySelector(".player-score").innerHTML += `
-    ${curPlayer.score}
-    `
   });
 })
 
+// Fetch player data (on a player deth) and manage DOM
 socket.on("playerDeath", (data) => {
   document.querySelector('#game-message').innerHTML = `The life of ${data.died.name} has been tragically removed by ${data.killedBy.name}`
   $("#game-message").css({
